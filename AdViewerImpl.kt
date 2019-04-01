a49259202889463ab89e8b4e7f1d822ce951129e
SeleznevAM <Seleznevam84@ya.ru>
2019-04-01 09:24:41 +0300

    Update AdViewerImpl.kt

--- AdViewerImpl.kt
+++ AdViewerImpl.kt
@@ -1,7 +1,7 @@
- override fun restoreAd() {
+override fun restoreAd() {
         GlobalScope.launch(Main) {
-            val view = viewDeferred.await()
-            if ((view as? View)?.parent != null) ((view as? View)?.parent as? ViewGroup)?.removeView(view as View)
+            val view = viewDeferred.await() ?: return@launch
+            if ((view as View).parent != null) ((view as View).parent as? ViewGroup)?.removeView(view as View)
             adSettings().inlineView().addView(view as View)
             view.play()
         }
